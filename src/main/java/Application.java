ddde32f727c4c9bec920b4146ed2ffe6ff5156bb
Koprvhdix <454898060@qq.com>
2016-11-18 23:28:19 +0800

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wukai on 16-11-18.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
