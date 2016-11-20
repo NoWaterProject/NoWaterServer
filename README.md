09253a69af17d3fe9991bdf7a5be3a09787e3f13
Koprvhdix <454898060@qq.com>
2016-11-20 08:29:48 +0800

    change port and improve README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NoWaterServer
 ## Usage
+下载好IDEA（最好是专业版，社区版没测试过）之后，选择File->Open->NoWaterServer目录下的pom.xml文件。然后就慢慢等它构建完，最下面状态栏没有Process在执行之后，Build->Make Project。
+装好最新版maven，配好环境变量。
 * mvn clean package
+* java -jar target/NoWaterServer-1.0-SNAPSHOT.jar
--- src/main/java/com/NoWater/Application.java
+++ src/main/java/com/NoWater/Application.java
@@ -2,14 +2,21 @@ package com.NoWater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 /**
  * Created by wukai on 16-11-18.
  */
 @SpringBootApplication
-public class Application {
+public class Application implements EmbeddedServletContainerCustomizer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8888);
     }
 }
