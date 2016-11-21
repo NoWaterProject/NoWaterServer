64713781d91cbed274c33b188349e3ea625bdbf5
Koprvhdix <koprvhdix@gmail.com>
2016-11-21 12:55:49 +0800

    add module dir

---
+++ src/main/java/com/NoWater/administrator/AdministratorLogin.java
@@ -0,0 +1,13 @@
+package com.NoWater.administrator;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wukai on 2016/11/21.
+ */
+@RestController
+public class AdministratorLogin {
+}
