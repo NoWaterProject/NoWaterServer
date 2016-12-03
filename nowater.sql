1f3f666a0808f7ccd9bb628f37fa1fcdaaa35f8b
Koprvhdix <koprvhdix@gmail.com>
2016-12-03 13:22:27 +0800

    change the databases

--- nowater.sql
+++ nowater.sql
@@ -1,6 +1,3 @@
--- ----------------------------
--- Table structure for `admin`
--- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `name` varchar(100) NOT NULL,
@@ -8,62 +5,68 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`name`)
 );
 
--- ----------------------------
--- Records of admin
--- ----------------------------
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
+  `shop_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_name` varchar(1024) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `email` varchar(1024) NOT NULL,
+  `status` int(11) NOT NULL,
+  `telephone` VARCHAR(1024) NOT NULL,
+  `shop_ad_photo_url` DEFAULT NULL,
+  PRIMARY KEY (`shop_id`)
+);
 
--- ----------------------------
--- Table structure for `comment_product`
--- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `product_name` varchar(1024) NOT NULL,
+  `ad_photo_url` varchar(1024) DEFAULT NULL,
+  `product_photo_url` varchar(1024) NOT NULL,
+  `default_size` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`)
+);
+
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE `class` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(1024) NOT NULL,
+  PRIMARY KEY (`class_id`)
+);
+
+DROP TABLE IF EXISTS `size_product`;
+CREATE TABLE `size_product` (
+  `size_id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(1024) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` double(15, 6) NOT NULL,
+  `photo_url` varchar(1024) NOT NULL,
+  `quantity_stock` int(11) NOT NULL,
+  `is_del` int(11) DEFAULT 0,
+  PRIMARY KEY (`size_id`)
+);
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `num` int(11) NOT NULL,
+  `size_id` int(11) NOT NULL,
+  PRIMARY KEY (`cart_id`)
+);
+
 DROP TABLE IF EXISTS `comment_product`;
 CREATE TABLE `comment_product` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `comment_content` varchar(1024) NOT NULL,
   `user_id` int(11) NOT NULL,
   `user_name` varchar(1024) NOT NULL,
+  `size_id` varchar()
   PRIMARY KEY (`comment_id`)
 );
 
--- ----------------------------
--- Records of comment_product
--- ----------------------------
-
--- ----------------------------
--- Table structure for `products`
--- ----------------------------
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
-  `class_id` int(11) NOT NULL,
-  `product_name` varchar(1024) NOT NULL,
-  `price` int(11) NOT NULL,
-  `photo_id` varchar(1024) NOT NULL,
-  `quantity_stock` int(11) NOT NULL,
-  PRIMARY KEY (`product_id`)
-);
-
--- ----------------------------
--- Records of products
--- ----------------------------
-
--- ----------------------------
--- Table structure for `size_product`
--- ----------------------------
-DROP TABLE IF EXISTS `size_product`;
-CREATE TABLE `size_product` (
-  `size_id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(1024) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  PRIMARY KEY (`size_id`)
-);
-
--- ----------------------------
--- Records of size_product
--- ----------------------------
-
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -73,30 +76,8 @@ CREATE TABLE `user` (
   `address1` varchar(1024) DEFAULT NULL,
   `address2` varchar(1024) DEFAULT NULL,
   `address3` varchar(1024) DEFAULT NULL,
+  `postCode` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 );
 
--- ----------------------------
--- Table structure for `cart`
--- ----------------------------
-DROP TABLE IF EXISTS `cart`;
-CREATE TABLE `cart` (
-  `cart_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `products_id` int(11) NOT NULL,
-  `num` int(11) NOT NULL,
-  `size_id` int(11) NOT NULL,
-  PRIMARY KEY (`cart_id`)
-);
-
-DROP TABLE IF EXISTS `shop`;
-CREATE TABLE `shop` (
-  `shop_id` int(11) NOT NULL AUTO_INCREMENT,
-  `shop_name` varchar(1024) DEFAULT NULL,
-  `owner_id` int(11) NOT NULL,
-  `email` varchar(1024) DEFAULT NULL,
-  `status` int(11) NOT NULL,
-  `telephone` VARCHAR(1024) NOT NULL,
-  PRIMARY KEY (`shop_id`)
-);
 
