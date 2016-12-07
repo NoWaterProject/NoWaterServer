ca842ce926c7f8b63ec91c478829a7319efe3970
Koprvhdix <koprvhdix@gmail.com>
2016-12-07 23:08:23 +0800

    add model product photo

--- src/main/java/com/NoWater/model/ProductPhoto.java
+++ src/main/java/com/NoWater/model/ProductPhoto.java
@@ -1,7 +1,63 @@
 package com.NoWater.model;
 
 /**
- * Created by Koprvhdix on 2016/12/7.
+ * Created by wukai on 2016/12/7.
  */
 public class ProductPhoto {
+    public void setFile_name(String file_name) {
+        this.file_name = file_name;
+    }
+
+    public void setPhoto_id(int photo_id) {
+        this.photo_id = photo_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFile_name() {
+
+        return file_name;
+    }
+
+    public int getPhoto_id() {
+        return photo_id;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    private String file_name;
+    private int photo_id;
+    private int product_id;
+    private String url;
+
+    public int getPhoto_type() {
+        return photo_type;
+    }
+
+    public int getIs_del() {
+        return is_del;
+    }
+
+    public void setPhoto_type(int photo_type) {
+        this.photo_type = photo_type;
+    }
+
+    public void setIs_del(int is_del) {
+        this.is_del = is_del;
+    }
+
+    private int photo_type;
+    private int is_del;
 }
