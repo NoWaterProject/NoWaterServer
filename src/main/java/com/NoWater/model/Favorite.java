0a6fdf33ce16a2831478032d08e84f310946969a
lpf372925 <pfl372925@gmail.com>
2016-12-28 15:46:18 +0800

    favorite

--- src/main/java/com/NoWater/model/Favorite.java
+++ src/main/java/com/NoWater/model/Favorite.java
@@ -4,24 +4,51 @@ package com.NoWater.model;
  * Created by qingpeng on 2016/12/8.
  */
 public class Favorite {
+    private int favorite_id;
+    private String favorite_time;
     private int user_id;
-    private int type;
+    private int favorite_type;
+    private int is_del;
     private int id;
 
-    public int getUser_id() {
+    public int getFavoriteId() {
+        return favorite_id;
+    }
+
+    public void setFavoriteId(int favorite_id) {
+        this.favorite_id = favorite_id;
+    }
+
+    public String getFavoriteTime() {
+        return favorite_time;
+    }
+
+    public void setFavoriteTime(String favorite_time) {
+        this.favorite_time = favorite_time;
+    }
+
+    public int getUserId() {
         return user_id;
     }
 
-    public void setUser_id(int user_id) {
+    public void setUserId(int user_id) {
         this.user_id = user_id;
     }
 
-    public int getType() {
-        return type;
+    public int getFavoriteType() {
+        return favorite_type;
     }
 
-    public void setType(int type) {
-        this.type = type;
+    public void setFavoriteType(int favorite_type) {
+        this.favorite_type = favorite_type;
+    }
+
+    public int getIsDel() {
+        return is_del;
+    }
+
+    public void setIsDel(int is_del) {
+        this.is_del = is_del;
     }
 
     public int getId() {
